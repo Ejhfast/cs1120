fb197f2de73aa6980f29a8bf831663a9206ebdc8
Paul DiOrio <paul@sine.(none)>
2009-10-26 16:35:51 -0400

    basic url structure for stories; errors fixed

--- stories/views.py
+++ stories/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("This will be a listing of all the stories.")
 	
-def detail(request):
+def detail(request, story_id):
 	return HttpResponse("You are looking at story %s." % story_id)
 	
-def comments(request):
-	return HttpResponse("This will be a listing of all the comments for stroy %s." % story_id)
+def comments(request, story_id):
+	return HttpResponse("This will be a listing of all the comments for story %s." % story_id)
