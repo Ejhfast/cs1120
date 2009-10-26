c9525fd83b236883dc2f25e3c1e1e8f572619637
Paul DiOrio <paul@sine.(none)>
2009-10-26 17:37:54 -0400

    detail has been modified to call detail.html template or throw 404 error

--- stories/views.py
+++ stories/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from cs1120.stories.models import Story
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
 	latest_story_list = Story.objects.all().order_by('-rating')[:20] # take the top 20 rated stories
 	return render_to_response('stories/index.html', {'latest_story_list':latest_story_list}) # loads index.html template with context of latest_story_list and renders the result
 
 def detail(request, story_id):
-	return HttpResponse("You are looking at story %s." % story_id)
+	s = get_object_or_404(Story, pk=story_id)
+	return render_to_response("stories/detail.html", {'story' :s})
 	
 def comments(request, story_id):
 	return HttpResponse("This will be a listing of all the comments for story %s." % story_id)
