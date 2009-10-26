b9f7003a0eff43d013e48987e563cf88640875ef
Paul DiOrio <paul@sine.(none)>
2009-10-26 16:24:43 -0400

    basic responses for stories

--- stories/views.py
+++ stories/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("This will be a listing of all the stories.")
+	
+def detail(request):
+	return HttpResponse("You are looking at story %s." % story_id)
+	
+def comments(request):
+	return HttpResponse("This will be a listing of all the comments for stroy %s." % story_id)
