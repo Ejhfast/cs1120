b02239992ceffe3589e32a0d604c8fda4e060b86
Paul DiOrio <paul@sine.(none)>
2009-10-26 16:44:53 -0400

    basic story display, ordered by rating

--- stories/views.py
+++ stories/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from cs1120.stories.models import Story
 
 def index(request):
-	return HttpResponse("This will be a listing of all the stories.")
+	latest_story_list = Story.objects.all().order_by('-rating')[:20] # take the top 20 rated stories
+	output = ', '.join([s.body for s in latest_story_list])
+	return HttpResponse(output)
 	
 def detail(request, story_id):
 	return HttpResponse("You are looking at story %s." % story_id)
