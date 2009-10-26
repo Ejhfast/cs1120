0f20264aca210681261ab6ff6e3a7e0a23115ebc
Paul DiOrio <paul@sine.(none)>
2009-10-26 16:51:42 -0400

    code now calls index.html to display stories

--- stories/views.py
+++ stories/views.py
@@ -4,14 +4,14 @@ from django.template import Context, loader
 
 def index(request):
 	latest_story_list = Story.objects.all().order_by('-rating')[:20] # take the top 20 rated stories
-	output = ', '.join([s.body for s in latest_story_list])
-	template = loader.get_template('stories/index.html')
-	c = Context({
-		'latest_story_list' : latest_story_list,
+	output = ', '.join([s.body for s in latest_story_list])	# seperate each story by a comma
+	template = loader.get_template('stories/index.html')	# load html template for display
+	context = Context({	
+		'latest_story_list' : latest_story_list,			# create new context with the 20 stories
 	})
 	
 	
-	return HttpResponse(t.render(c))
+	return HttpResponse(template.render(context))	# render the context (list of stories) in the template
 	
 def detail(request, story_id):
 	return HttpResponse("You are looking at story %s." % story_id)
