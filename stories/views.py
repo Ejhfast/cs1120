eb5a0974069d2a54fb1e1a119887833d315d22cc
Ethan Fast <ethan@d-172-26-5-14.bootp.virginia.edu>
2009-10-27 09:01:52 -0400

    login required to upvote/downvote/newstory

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
@@ -13,25 +14,31 @@ def detail(request, story_id):
 	return render_to_response("stories/detail.html", {'story' :s})
 	
 def upvote(request, story_id):
-	s = get_object_or_404(Story, pk=story_id)
-	s.upvotes += 1
-	s.save()
+	if request.user.is_authenticated():
+		s = get_object_or_404(Story, pk=story_id)
+		s.upvotes += 1
+		s.save()
 	return render_to_response("stories/vote.html", {'story_id' :story_id, 'type' : 'upvote', 'story':s})
 	
 def downvote(request, story_id):
-	s = get_object_or_404(Story, pk=story_id)
-	s.downvotes += 1
-	s.save()
+	if request.user.is_authenticated():
+		s = get_object_or_404(Story, pk=story_id)
+		s.downvotes += 1
+		s.save()
 	return render_to_response("stories/vote.html", {'story_id' :story_id, 'type' : 'downvote', 'story' : s})
 	
 def newpost(request):
-	s = StoryForm()
-	return render_to_response('stories/addstory.html', {'story_form' : s})
+	if request.user.is_authenticated():
+		s = StoryForm()
+		return render_to_response('stories/addstory.html', {'story_form' : s})
+	else:
+		return HttpResponseRedirect("/posts/")
 
 def addpost(request):
-	s = StoryForm(request.POST)
-	s.save()
-	latest_story_list = Story.objects.all().order_by('-upvotes')[:20] 
+	if request.user.is_authenticated():
+		s = StoryForm(request.POST)
+		s.save()
+		latest_story_list = Story.objects.all().order_by('-upvotes')[:20] 
 	return render_to_response('stories/index.html', {'latest_story_list':latest_story_list}) 
 
 def newuser(request):
